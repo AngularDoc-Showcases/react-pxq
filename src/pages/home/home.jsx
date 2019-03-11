7d542b5bf5668a68e069643fcdc3c7719f3fe403
Hanyu Xiao <hanyu.xiao@gmail.com>
2019-03-11 01:29:45 -0700

    removed package alias from home.jsx to allow the actions to be resolved

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,15 +5,15 @@ import { is, fromJS } from 'immutable';
 import PropTypes from 'prop-types';
 import API from '@/api/api';
 import envconfig from '@/envconfig/envconfig';
-import { saveFormData, saveImg, clearData } from '@/store/home/action';
-import { clearSelected } from '@/store/production/action';
+import { saveFormData, saveImg, clearData } from '../../store/home/action';
+import { clearSelected } from '../../store/production/action';
 import PublicHeader from '@/components/header/header';
 import PublicAlert from '@/components/alert/alert';
 import TouchableOpacity from '@/components/TouchableOpacity/TouchableOpacity';
 import mixin, { padStr } from '@/utils/mixin';
 import './home.less';
 
-@mixin({padStr})
+@mixin({ padStr })
 class Home extends Component {
   static propTypes = {
     formData: PropTypes.object.isRequired,
@@ -31,7 +31,7 @@ class Home extends Component {
    * 已选择的商品数据
    * @type {Array}
    */
-  selectedProList = []; 
+  selectedProList = [];
 
   /**
    * 将表单数据保存至redux，保留状态
@@ -40,46 +40,46 @@ class Home extends Component {
    */
   handleInput = (type, event) => {
     let value = event.target.value;
-    switch(type){
+    switch (type) {
       case 'orderSum':
         value = value.replace(/\D/g, '');
-      break;
+        break;
       case 'name':
-      break;
+        break;
       case 'phoneNo':
         value = this.padStr(value.replace(/\D/g, ''), [3, 7], ' ', event.target);
-      break;
-      default:;
+        break;
+      default: ;
     }
     this.props.saveFormData(value, type);
   }
-  
+
   /*
   上传图片，并将图片地址存到redux，保留状态
    */
   uploadImg = async event => {
-    try{
+    try {
       let formdata = new FormData();
       formdata.append('file', event.target.files[0]);
-      let result = await API.uploadImg({data: formdata});
+      let result = await API.uploadImg({ data: formdata });
       this.props.saveImg(envconfig.imgUrl + result.image_path);
       console.log(result);
-    }catch(err){
+    } catch (err) {
       console.error(err);
     }
   }
 
   // 提交表单
   sumitForm = () => {
-    const {orderSum, name, phoneNo} = this.props.formData;
+    const { orderSum, name, phoneNo } = this.props.formData;
     let alertTip = '';
-    if(!orderSum.toString().length){
+    if (!orderSum.toString().length) {
       alertTip = '请填写金额';
-    }else if(!name.toString().length){
+    } else if (!name.toString().length) {
       alertTip = '请填写姓名';
-    }else if(!phoneNo.toString().length){
+    } else if (!phoneNo.toString().length) {
       alertTip = '请填写正确的手机号';
-    }else{
+    } else {
       alertTip = '添加数据成功';
       this.props.clearSelected();
       this.props.clearData();
@@ -89,7 +89,7 @@ class Home extends Component {
       alertTip,
     })
   }
-  
+
   // 关闭弹款
   closeAlert = () => {
     this.setState({
@@ -97,34 +97,34 @@ class Home extends Component {
       alertTip: '',
     })
   }
-  
+
   // 初始化数据，获取已选择的商品
   initData = props => {
     this.selectedProList = [];
     props.proData.dataList.forEach(item => {
-      if(item.selectStatus && item.selectNum){
+      if (item.selectStatus && item.selectNum) {
         this.selectedProList.push(item);
       }
     })
   }
 
-  componentWillReceiveProps(nextProps){
-    if(!is(fromJS(this.props.proData), fromJS(nextProps.proData))){
+  componentWillReceiveProps(nextProps) {
+    if (!is(fromJS(this.props.proData), fromJS(nextProps.proData))) {
       this.initData(nextProps);
     }
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
+    return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state), fromJS(nextState))
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.initData(this.props);
   }
-  
+
 
   render() {
-    
+
     return (
       <main className="home-container">
         <PublicHeader title='首页' record />
@@ -132,15 +132,15 @@ class Home extends Component {
         <form className="home-form">
           <div className="home-form-tiem">
             <span>销售金额：</span>
-            <input type="text" placeholder="请输入订单金额" value={this.props.formData.orderSum} onChange={this.handleInput.bind(this, 'orderSum')}/>
+            <input type="text" placeholder="请输入订单金额" value={this.props.formData.orderSum} onChange={this.handleInput.bind(this, 'orderSum')} />
           </div>
           <div className="home-form-tiem">
             <span>客户姓名：</span>
-            <input type="text" placeholder="请输入客户姓名" value={this.props.formData.name} onChange={this.handleInput.bind(this, 'name')}/>
+            <input type="text" placeholder="请输入客户姓名" value={this.props.formData.name} onChange={this.handleInput.bind(this, 'name')} />
           </div>
           <div className="home-form-tiem">
             <span>客户电话：</span>
-            <input type="text" maxLength="13" placeholder="请输入客户电话" value={this.props.formData.phoneNo} onChange={this.handleInput.bind(this, 'phoneNo')}/>
+            <input type="text" maxLength="13" placeholder="请输入客户电话" value={this.props.formData.phoneNo} onChange={this.handleInput.bind(this, 'phoneNo')} />
           </div>
         </form>
         <div>
@@ -153,7 +153,7 @@ class Home extends Component {
                     return <li key={index} className="selected-pro-item ellipsis">{item.product_name}x{item.selectNum}</li>
                   })
                 }
-              </ul>:'选择产品'
+              </ul> : '选择产品'
             }
           </Link>
         </div>
@@ -161,9 +161,9 @@ class Home extends Component {
           <p className="common-title">请上传发票凭证</p>
           <div className="file-lable">
             <span className="common-select-btn">上传图片</span>
-            <input type="file" onChange={this.uploadImg}/>
+            <input type="file" onChange={this.uploadImg} />
           </div>
-          <img src={this.props.formData.imgpath} className="select-img" alt=""/>
+          <img src={this.props.formData.imgpath} className="select-img" alt="" />
         </div>
         <TouchableOpacity className="submit-btn" clickCallBack={this.sumitForm} text="提交" />
         <PublicAlert closeAlert={this.closeAlert} alertTip={this.state.alertTip} alertStatus={this.state.alertStatus} />
@@ -176,8 +176,8 @@ export default connect(state => ({
   formData: state.formData,
   proData: state.proData,
 }), {
-  saveFormData, 
-  saveImg,
-  clearData,
-  clearSelected,
-})(Home);
+    saveFormData,
+    saveImg,
+    clearData,
+    clearSelected,
+  })(Home);
